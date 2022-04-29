a7622a287fdd965dcccfae1c3236b372dcb84658
Your Name <you@example.com>
2022-04-28 22:07:40 -0400

    solo comentarios

--- JS/jsindex.js
+++ JS/jsindex.js
@@ -1,16 +1,29 @@
 //Contraseña
  document
-  .getElementById('contrasena')
+  .getElementById('contraseña')
   .addEventListener('input', function(evt) {
-    const contrasena = evt.target,
-          valido = document.getElementById('contrasenaOK'),
+    const contraseña = evt.target,
+          valido = document.getElementById('contraseñaOK'),
         
           alfanum = /^(?=.*\d)(?=.*[a-záéíóúüñ]).*[A-ZÁÉÍÓÚÜÑ]/;
 
     //Se muestra un texto válido/inválido a modo de ejemplo
-    if (alfanum.test(contrasena.value)) {
+    if (alfanum.test(contraseña.value)) {
       valido.innerText = "válido";
     } else {
       valido.innerText = "incorrecto";
     }
   });
+
+
+  //CHECK BOX -- GUARDAR CONTRASEÑA
+
+const guardarSwitch = document.querySelector('#switch input[type="checkbox"]');
+          function guardarContraseña(ev){
+              if(ev.target.checked){
+                  document.documentElement.setAttribute('tema', 'light');
+              } else {
+                  document.documentElement.setAttribute('tema', 'dark');
+              }
+          }
+          guardarSwitch.addEventListener('change', guardarContraseña);
